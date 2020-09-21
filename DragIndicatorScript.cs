6f12102f4ca9ff7e406c948b7ecaa8126c6f1d2a
Dennis Engebrigtsen <62187022+Engabrigadiga@users.noreply.github.com>
2020-09-21 16:53:00 -0400

    Add files via upload

---
+++ DragIndicatorScript.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragIndicatorScript : MonoBehaviour
+{
+    Vector3 startPos;
+    Vector3 endPos;
+    Camera camera;
+    LineRenderer lr;
+
+    public float lineLength; // to adjust force
+    public float lineAngle;
+
+    [SerializeField]
+    private float applyForce = 20f;
+
+    Vector3 camOffset = new Vector3(0, 0, 500); //???
+
+    [SerializeField] AnimationCurve ac;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) // might need to be changed for mobile
+        {
+            //if linerenderer doesnt exist create it
+            if (lr == null)
+            {
+                lr = gameObject.AddComponent<LineRenderer>();
+            }
+            lr.enabled = true;
+            lr.sortingLayerName = "Front";
+          
+           
+            
+            //we want a line with two points
+            lr.positionCount = 2;
+            //to get our mouse position to our 2d world
+            startPos = camera.ScreenToWorldPoint(Input.mousePosition) + camOffset;
+            lr.SetPosition(0, startPos);
+            lr.useWorldSpace = true; // Disconnect click position from local position of game object
+            lr.widthCurve = ac;
+            lr.numCapVertices = 10;
+        }
+
+
+
+        if (Input.GetMouseButton(0))
+        {
+            endPos = camera.ScreenToWorldPoint(Input.mousePosition) + camOffset;
+            lr.SetPosition(1, endPos);
+        }
+
+        //calculate the angle of the line
+        float angle = (Mathf.Atan2(endPos.y-startPos.y, endPos.x-startPos.x)*180 / Mathf.PI);
+        
+       // Debug.Log( "The Angle is:" + angle);
+        
+        // take points from vector to calculate angle that force will be applied
+        float length = ((startPos.x - endPos.x) * (startPos.x - endPos.x) +
+                        (startPos.y - endPos.y) * (startPos.y - endPos.y)); // calculate the length of line and convert that into force
+    
+       // Debug.Log( "The Length is:" + length);
+        
+      
+        if (Input.GetMouseButtonUp(0))
+        {
+           
+            lineLength = length;
+            lineAngle = angle; 
+            lr.enabled = false;
+            RigidBodyMovementTest movementTest = new RigidBodyMovementTest();
+
+        }
+
+        
+
+    }
+}
---
+++ RigidBodyMovementTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidBodyMovementTest : MonoBehaviour
+{
+        
+    public GameObject DragIndcator;
+    private DragIndicatorScript _dragIndicatorScript;
+    private Vector3 dir;
+    
+    
+    private float speed;
+    public Vector2 movement;
+    public Rigidbody2D rb2d;
+    void Start()
+    {
+        rb2d = this.GetComponent<Rigidbody2D>();
+    }
+
+    private void Awake()
+    {
+        _dragIndicatorScript = DragIndcator.GetComponent<DragIndicatorScript>();
+    }
+
+    void Update()
+    {
+        //get a vector of the line drawn
+        speed = _dragIndicatorScript.lineLength;
+        
+        Debug.Log("PLEASE HAVE VALUES: " + _dragIndicatorScript.lineLength + "\t" + _dragIndicatorScript.lineAngle);
+        
+        //movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        Debug.Log( "movement vector is: "+movement);
+
+       // float angle = _dragIndicatorScript.lineAngle;
+        dir = Quaternion.AngleAxis(_dragIndicatorScript.lineAngle, Vector3.forward) * Vector3.right;
+    }
+
+    private void FixedUpdate() //is run every psysics step instead of every frame
+    {
+        MoveCharacter(movement);
+    }
+
+
+    void MoveCharacter(Vector2 direction)
+    {
+        /* Ways to move our object with rigid body: 
+         1)RigidBody.AddForce()
+                Adds a force to a object
+         2)RigidBody.velocity
+         3)RigidBody.movePosition
+         */
+        
+        Vector2 vertTest = new Vector2(direction.x,(direction.y) * 10);
+
+         rb2d.AddForce(dir * speed);
+
+         //rb2d.velocity = direction * speed;
+         // rb2d.MovePosition((Vector2)transform.position + (direction * speed * Time.deltaTime));
+    }
+}
